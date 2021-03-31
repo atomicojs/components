25d4fd88585eb1ea10bf93da4678726dd397560c
UpperCod <mtrujillo@outlook.cl>
2021-03-31 00:12:19 -0300

    try the new export format for ts

---
+++ element-modal.d.ts
@@ -0,0 +1,3 @@
+export { Modal } from "./src/components/modal/element-modal.jsx";
+
+declare module "./modal";
--- modal.d.ts
+++
@@ -1,3 +0,0 @@
-export { Modal } from "./src/components/modal/modal.jsx";
-
-declare module "./tag-modal";
