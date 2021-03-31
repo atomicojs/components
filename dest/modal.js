bda5e81699b914206d14446a0b0fc38a8422aa22
UpperCod <mtrujillo@outlook.cl>
2021-03-31 00:13:16 -0300

    update ignore

--- dest/modal.js
+++ dest/modal.js
@@ -1,6 +1,1 @@
-import { Modal as o } from "./element-modal.js";
-import "atomico/jsx-runtime";
-import "atomico";
-import "@atomico/hooks/use-responsive-state";
-import "@atomico/hooks/use-resize-observer";
-customElements.define("a-modal", o);
+import{Modal as o}from"./element-modal.js";import"atomico/jsx-runtime";import"atomico";import"@atomico/hooks/use-responsive-state";import"@atomico/hooks/use-resize-observer";customElements.define("a-modal",o);
