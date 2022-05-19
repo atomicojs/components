1337e85063401860dc42692750fae31271358c64
UpperCod <mtrujillo@outlook.cl>
2022-05-18 23:38:11 -0400

    update components

---
+++ src/keen-slider/README.md
@@ -0,0 +1,20 @@
+---
+title: ./keen-slider.tsx
+---
+
+```js script
+import "./keen-slider.tsx";
+import { Stories, Story } from "@atomico/stories";
+```
+
+```html preview
+<atomico-keen-slider></atomico-keen-slider>
+```
+
+```jsx
+<Stories>
+    <Story>
+        <button> my button </button>
+    </Story>
+</Stories>
+```
--- src/keen-slider/keen-slider.tsx
+++ src/keen-slider/keen-slider.tsx
@@ -1,226 +1,87 @@
-import {
-    Host,
-    Props,
-    c,
-    useEffect,
-    useProp,
-    css,
-    useHost,
-    useRef,
-} from "atomico";
-import { useChildNodes } from "@atomico/hooks/use-child-nodes";
-import { useMutationObserver } from "@atomico/hooks/use-mutation-observer";
-import Keen from "keen-slider";
+import { Type, Props, c, css, useRef, useEffect } from "atomico";
+import Keen, { KeenSliderInstance } from "keen-slider";
+import { useProxySlot } from "@atomico/hooks/use-slot";
 import style from "./keen-slider.css";
+import { useResponsiveState } from "@atomico/hooks/use-responsive-state";
 
-function component(
-    props: Props<typeof component>
-): Host<{ onSliderMounted: Event }> {
-    const host = useHost();
-    const refSlider = useRef();
-    const [childNodes, update] = useChildNodes();
-    const children = childNodes.filter((child) =>
-        child instanceof HTMLElement
-            ? props.selector
-                ? child.matches(props.selector)
-                : !child.slot || child.slot.startsWith("slide-")
-            : false
-    ) as HTMLElement[];
+function component(props: Props<typeof component>) {
+    const refSlide = useRef<KeenSliderInstance>();
+    const refRoot = useRef();
+    const refSlides = useRef();
+    const slotSlides = useProxySlot<HTMLElement>(refSlides);
 
-    useMutationObserver(host, update, { childList: true });
-
-    const [slider, setSlider] = useProp("slider");
-    const [hover, setHover] = useProp<boolean>("hover");
-    const [currentIndex, setCurrentIndex] = useProp<number>("currentSlide");
+    const slidesPerView = useResponsiveState(props.slidesPerView || "");
+    const slidesSpacing = useResponsiveState(props.slidesPerView || "");
+    const slidesOrigin = useResponsiveState(props.slidesOrigin || "");
 
     useEffect(() => {
-        const slider = new Keen(refSlider.current, {
-            slideChanged(slider) {
-                setCurrentIndex(slider.track.details.rel);
+        if (!slotSlides.length) return;
+        const slider = new Keen(refRoot.current, {
+            loop: props.loop,
+            drag: props.drag,
+            disabled: props.disabled,
+            rtl: props.rtl,
+            rubberband: props.rubberband,
+            initial: props.initial,
+            mode: props.mode,
+            vertical: !!props.vertical,
+            slides: {
+                perView: slidesPerView,
+                spacing: slidesSpacing,
+                // origin: slidesOrigin,
             },
-            ...Object.entries({
-                vertical: props.vertical,
-                breakpoints: props.breakpoints,
-                duration: props.duration,
-                drag: props.drag,
-                dragSpeed: props.dragSpeed,
-                initial: props.initial,
-                loop: props.loop || props.autoplay,
-                mode: props.mode,
-                rtl: props.rtl,
-                rubberband: props.rubberband,
-                slides: [
-                    ["perView", props.slidesPerView],
-                    ["spacing", props.slidesSpacing],
-                    ["origin", props.slidesOrigin],
-                ]
-                    .filter(([, value]) => value != null)
-                    .reduce(
-                        (props, [prop, value]) =>
-                            //@ts-ignore
-                            ({ ...props, [prop]: value }),
-                        {}
-                    ),
-            })
-                .filter(([, value]) => value != null)
-                .reduce(
-                    (props, [prop, value]) => ({
-                        ...props,
-                        [prop]: value,
-                    }),
-                    {}
-                ),
         });
 
-        setSlider(slider);
-    }, []);
+        refSlide.current = slider;
 
-    useEffect(() => {
-        if (!slider || !props.autoplay) return;
+        return () => slider.destroy();
+    }, [slotSlides.length, slidesPerView, slidesSpacing, slidesOrigin]);
 
-        if (!hover) {
-            const interval = setInterval(
-                () => slider.next(),
-                props.autoplayLoop
-            );
-            return () => clearInterval(interval);
-        }
-    }, [slider, hover, props.autoplay, props.autoplayLoop]);
+    const next = () => {
+        refSlide.current.next();
+    };
+    const prev = () => {
+        refSlide.current.prev();
+    };
 
     return (
-        <host
-            shadowDom
-            onmouseover={() => setHover(true)}
-            onmouseleave={() => setHover(false)}
-        >
-            <div class="keen-slider" ref={refSlider}>
-                {children.map((el, index) => (
-                    <div class="keen-slider__slide">
-                        <slot name={(el.slot = "slide-" + index)}></slot>
-                    </div>
-                ))}
+        <host shadowDom next={next} prev={prev}>
+            <slot ref={refSlides} name="slide"></slot>
+            <slot onclick={prev} name="to-left"></slot>
+            <slot onclick={next} name="to-right"></slot>
+            <div class="keen-slider" ref={refRoot}>
+                {slotSlides.map((element, id) => {
+                    const name = `slide-${id}`;
+                    element.slot = name;
+                    return (
+                        <div class="keen-slider__slide">
+                            <slot name={name}></slot>
+                        </div>
+                    );
+                })}
             </div>
-            <slot part="to-left" name="to-left" onclick={slider?.prev}></slot>
-            <slot part="to-right" name="to-right" onclick={slider?.next}></slot>
-            {props.dots && (
-                <div
-                    part="dots"
-                    class="dots"
-                    style={`--dots-length: ${children.length}`}
-                >
-                    {children.map((el, index) => (
-                        <button
-                            part="dots-item"
-                            class={`dots-item ${
-                                currentIndex === index
-                                    ? "dots-item--active"
-                                    : ""
-                            }`}
-                            onclick={() => slider?.moveToIdx(index)}
-                        ></button>
-                    ))}
-                </div>
-            )}
         </host>
     );
 }
 
 component.props = {
-    currentSlide: {
-        type: Number,
-        value: 0,
-    },
-    breakpoints: Object,
-    duration: Number,
-    disabled: Boolean,
-    drag: Boolean,
-    dragSpeed: Number,
-    initial: Number,
     loop: Boolean,
-    autoplay: Boolean,
-    autoplayLoop: {
-        type: Number,
-        value: 2000,
-    },
-    slidesPerView: Number,
-    slidesSpacing: Number,
-    slidesOrigin: String,
-    dots: Boolean,
-    hover: {
-        type: Boolean,
-        reflect: true,
-    },
+    drag: Boolean,
+    disabled: Boolean,
+    vertical: Boolean,
+    rtl: Boolean,
+    rubberband: Boolean,
+    initial: Number,
     mode: {
         type: String,
         value: (): "snap" | "free" | "free-snap" => "snap",
     },
-    range: {
-        type: Object,
-        value: (): { min?: number; max?: number; align?: boolean } => ({}),
-    },
-    rtl: Boolean,
-    rubberband: Boolean,
-    selector: String,
-    vertical: { type: Boolean, reflect: true },
-    slider: {
-        type: Object,
-        event: { type: "SliderMounted" },
-    },
+    slidesPerView: String,
+    slidesSpacing: String,
+    slidesOrigin: String,
 };
 
-component.styles = [
-    style,
-    css`
-        :host {
-            position: relative;
-            display: block;
-            --dots-gap: 1rem;
-            --dots-opacity: 0.5;
-            --dots-transition: 0.3s ease all;
-        }
-        ::slotted([slot="to-left"]),
-        ::slotted([slot="to-right"]) {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            cursor: pointer;
-            z-index: 1;
-        }
-        ::slotted([slot="to-left"]) {
-            left: 0;
-        }
-        ::slotted([slot="to-right"]) {
-            right: 0;
-        }
-        .dots {
-            position: absolute;
-            bottom: 0px;
-            left: 50%;
-            z-index: 2;
-            display: grid;
-            grid-template-columns: repeat(var(--dots-length), auto);
-            grid-gap: var(--dots-gap);
-            transform: translate(-50%, -100%);
-        }
-        .dots-item {
-            width: 10px;
-            height: 10px;
-            padding: 0px;
-            border-radius: 100px;
-            border: none;
-            color: white;
-            cursor: pointer;
-            opacity: var(--dots-opacity);
-            transition: var(--dots-transition);
-        }
-        .dots-item--active {
-            --dots-opacity: 1;
-        }
-    `,
-];
+component.styles = style;
 
 export const KeenSlider = c(component);
 
